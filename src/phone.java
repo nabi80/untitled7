206419c688f6287c79673f6ea8afc0611bf0f5a6
nabi80 <sarfarazinabi@gmail.com>
2023-02-01 19:41:20 -0500

    vvvv

---
+++ src/phone.java
@@ -0,0 +1,48 @@
+public class phone {
+
+    String brand;
+    String color;
+    int price;
+    int version;
+    double storage;
+    boolean isonlooked;
+
+    void ringing() {
+        System.out.println("ringing ringing");
+    }
+
+    void sendingmessage() {
+        System.out.println("sms");
+    }
+
+    void calling() {
+        System.out.println("recieveing calls");
+    }
+
+    void usingGps() {
+        System.out.println("showing map");
+    }
+
+    void playinggame() {
+        System.out.println("video game");
+    }
+
+    void checkingandsendingemail() {
+        System.out.println("email notification");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
